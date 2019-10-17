f939c2f079946c065366c40cbc0f2f9e973689a5
Nish6496 <36617684+Nimra-Sharnez@users.noreply.github.com>
2019-10-16 20:37:20 -0600

    Update Customer.java

--- HardwareStoreSimulation/Customer.java
+++ HardwareStoreSimulation/Customer.java
@@ -1,4 +1,10 @@
 package HardwareStoreSimulation;
 
 public class Customer {
+      public String getCustomerName() {
+        return this.name;
+    }
+      public String getCustomerType() {
+        return this.type;
+    }
 }
